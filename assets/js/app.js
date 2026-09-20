d0a225e347b06af0c0b396ff8cca4df8e39cd6a9
juliobugfixer <ngmanuel21@gmail.com>
2026-09-21 00:16:34 +0100

    feat: Implements button's event

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+const decimal = document.querySelector("#decimalNumber");
+const binary = document.querySelector("#binaryResult");
+const button = document.querySelector("#button");
+
+debugger;
+button.addEventListener("click", () => {
+  console.log("Funcionou!");
+});
